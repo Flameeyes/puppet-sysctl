c8660f2c91bff0fb7860d8d7fc88c1a941d31c75
Diego Elio Pettenò <flameeyes@flameeyes.eu>
2013-03-25 04:32:08 -0700

    Define sysctl class if it was not.

--- manifests/value.pp
+++ manifests/value.pp
@@ -1,4 +1,8 @@
 define sysctl::value($content) {
+  if ! defined( Class["sysctl"] ) {
+    class { sysctl: }
+  }
+
   concat::fragment { "sysctl_$name":
     target => '/etc/sysctl.conf',
     content => "${name} = ${content}\n",
